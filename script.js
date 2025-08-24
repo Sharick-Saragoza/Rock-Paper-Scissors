71bcd58d3e8cbfde330e8b7cefc654ca5bc9b3ca
Sharick-Saragoza <150659105+Sharick-Saragoza@users.noreply.github.com>
2025-08-25 00:06:20 +0200

    Function getHumanChoice created that grabs the input from the human.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Function randomly returns “rock”, “paper” or “scissors”.
 function getComputerChoice(computerChoice) {
 
+    // Generate a random value from 0 to 2.
     let randomValue = Math.floor(Math.random() * 3);
 
     if (randomValue === 0) {
@@ -8,6 +9,15 @@ function getComputerChoice(computerChoice) {
     } else if (randomValue === 1) {
         computerChoice = "paper";
     } else {
-        computerChoice = "scissors"
+        computerChoice = "scissors";
     }
+
+    // console.log(computerChoice, randomValue)
 }
+
+function getHumanChoice(humanChoice) {
+    humanChoice = prompt("Choose rock, paper or scissors", "");
+    console.log(humanChoice);
+}
+
+// getHumanChoice()
