46b4b2c2fff42b875ea4f77f90a0471bcf8269cf
Sharick-Saragoza <150659105+Sharick-Saragoza@users.noreply.github.com>
2025-08-25 14:17:28 +0200

    The playRound function has been created and it looks for the winner.

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
 let computerScore = 0;
 let humanScore = 0;
 
-// Function randomly returns “rock”, “paper” or “scissors”.
+// Returns a random choice “rock”, “paper” or “scissors”.
 function getComputerChoice(computerChoice) {
 
     // Generate a random value from 0 to 2.
     let randomValue = Math.floor(Math.random() * 3);
-
+    
     if (randomValue === 0) {
-        computerChoice = "rock";
+        return computerChoice = "rock";
     } else if (randomValue === 1) {
-        computerChoice = "paper";
+        return computerChoice = "paper";
     } else {
-        computerChoice = "scissors";
+        return computerChoice = "scissors";
     }
-
-    // console.log(computerChoice, randomValue)
+    
 }
 
+// Gets the input of the human
 function getHumanChoice(humanChoice) {
-    humanChoice = prompt("Choose rock, paper or scissors", "");
-    console.log(humanChoice);
+    return humanChoice = prompt("Choose rock, paper or scissors", "");
 }
 
-// getHumanChoice()
+function playRound() {
+
+    if (getComputerChoice() === "rock" && getHumanChoice() === "paper") {
+        console.log("You win paper wins against rock!");
+    } else if (getComputerChoice() === "rock" && getHumanChoice() === "scissors") {
+        console.log("You lose and the scissors break!");
+    } else if (getComputerChoice() === "paper" && getHumanChoice() === "rock") {
+        console.log("You lose rock loses against paper!");
+    } else if (getComputerChoice() === "paper" && getHumanChoice() === "scissors") {
+        console.log("You win scissors wins against paper!");
+    } else if (getComputerChoice() === "scissors" && getHumanChoice() === "rock") {
+        console.log("You win rock wins against scissors");
+    } else if (getComputerChoice() === "scissors" && getHumanChoice() === "paper") {
+        console.log("You lose paper loses against scissors")
+    } else if (getComputerChoice() === getHumanChoice()) {
+        console.log("Draw, no one wins")
+    }
+}
+
+// console.log("You played " + getHumanChoice() + " against " + getComputerChoice());
